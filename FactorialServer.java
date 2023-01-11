601eebdcb80aa9e2ef0be84da3a2b28d6d0a253c
Miltiadis Maleas <120583796+Miltos717@users.noreply.github.com>
2023-01-11 19:45:39 +0200

    Update FactorialServer.java

--- FactorialServer.java
+++ FactorialServer.java
@@ -12,34 +12,34 @@ public class FactorialServer {
 	
 	public static void main(String[] args) throws Exception {
 				
-		// Should be first, especially if server is NOT localhost
+        // Should be first, especially if server is NOT localhost
         System.setProperty("java.rmi.server.hostname", HOST);
                 
         // Remote object creation
-		Factorial robj = new FactorialImpl();
+	Factorial robj = new FactorialImpl();
 		
-		// Choose the appropriate method:
+	// Choose the appropriate method:
         // get if rmi registry is already running, create otherwise 
-		// Registry registry = LocateRegistry.getRegistry(HOST, PORT);
-		Registry registry = LocateRegistry.createRegistry(PORT);
+	// Registry registry = LocateRegistry.getRegistry(HOST, PORT);
+	Registry registry = LocateRegistry.createRegistry(PORT);
 		
-		// Bind remote object to a name and publish in rmi registry
+	// Bind remote object to a name and publish in rmi registry
                 
-		String rmiObjectName = "Factorial";
-		registry.rebind(rmiObjectName, robj);
-		System.out.println("Remote object bounded.");
+	String rmiObjectName = "Factorial";
+	registry.rebind(rmiObjectName, robj);
+	System.out.println("Remote object bounded.");
 		
 		
-		// Server is running until we press a key
+	// Server is running until we press a key
 		
-		System.out.println("Press <Enter> for exit.");
-		System.in.read();
+	System.out.println("Press <Enter> for exit.");
+	System.in.read();
 		
-		// Free space and clear rmi registry
+	// Free space and clear rmi registry
 		
-		UnicastRemoteObject.unexportObject(robj, true);
-		registry.unbind(rmiObjectName);
-		System.out.println("Remote object unbounded.");
+	UnicastRemoteObject.unexportObject(robj, true);
+	registry.unbind(rmiObjectName);
+	System.out.println("Remote object unbounded.");
 	}
 }
 
